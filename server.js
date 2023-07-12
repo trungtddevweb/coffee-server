c599f8e07a268307fc4779b5ce363ea28ee9af63
Trung <109146090+trungtddevweb@users.noreply.github.com>
2023-07-12 17:51:33 +0700

    cors

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import morgan from 'morgan'
 import helmet from 'helmet'
 import bodyParser from 'body-parser'
 
-import { DB_URL, PORT, SESSION_KEY } from './src/utils/const.js'
+import { PORT } from './src/utils/const.js'
 import appRoutes from './src/routes/index.js'
 import connectDB from './connect.js'
 
@@ -13,14 +13,28 @@ const app = express()
 
 // Middlewawre
 app.use(express.json())
-app.use(
-    cors({
-        origin: ['http://localhost:5173', 'https://coffee-sweet.vercel.app/'],
-        optionsSuccessStatus: 200,
-        credentials: true,
-        exposedHeaders: ['set-cookie'],
-    })
-)
+app.use((req, res, next) => {
+    const allowedOrigins = [
+        'http://localhost:5173',
+        'https://coffee-sweet.vercel.app',
+    ]
+    const origin = req.headers.origin
+
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin)
+    }
+
+    res.setHeader('Access-Control-Allow-Credentials', 'true')
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, OPTIONS, PATCH, PUT'
+    )
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    )
+    next()
+})
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }))
 app.use(morgan('combined'))
