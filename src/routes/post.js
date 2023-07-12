36f77bf1402f5fe8d6a8ea801111456f135ef1f7
Trung <109146090+trungtddevweb@users.noreply.github.com>
2023-07-12 12:34:59 +0700

    auth

--- src/routes/post.js
+++ src/routes/post.js
@@ -1,10 +1,13 @@
 import express from 'express'
-import { signIn, signUp, signOut } from '../controllers/auth.js'
-
+import { createPost, getAllPost } from '../controllers/post.js'
+import { verifyUser } from '../middlewares/verify.js'
+import { uploadCloudPost } from '../middlewares/cloudinary.js'
 const router = express.Router()
 
-// router.post('/sign-in', signIn)
-// router.post('/sign-up', signUp)
+router.post('/create', verifyUser, uploadCloudPost.single('image'), createPost)
+
+router.get('/all-post', getAllPost)
+
 // router.post('/sign-out', signOut)
 
 export default router
